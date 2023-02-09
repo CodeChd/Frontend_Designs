99d66f39864466a952097569cd0823980187df4e
CodeChd <Drowzyking1126@gmail.com>
2023-02-10 04:11:20 +0800

    Update Main Section

--- components/Content.jsx
+++ components/Content.jsx
@@ -12,7 +12,10 @@ export default function Content() {
                     </div>
 
                     <div className="details-box">
+                    <div className="txt-wrapper">
                         <h2>The Bright Future  of Web 3.0?</h2>
+                    </div>
+
 
                         <div className="desc">
                             <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
@@ -25,20 +28,22 @@ export default function Content() {
                 </div>
 
                 <div className="col-2">
+                   
+
                     <h2>New</h2>
 
-                    <di className="info">
+                    <div className="info">
                         <h3>Hydrogen Vs Electric Cars</h3>
                         <p>will hydrogen-fueled cars ever catch up to Evs? </p>
-                    </di>
-                    <di className="info">
+                    </div>
+                    <div className="info">
                         <h3>The Downside of AI Artistry</h3>
                         <p>What are the possible adverse effects of on-demand AI image generation? </p>
-                    </di>
-                    <di className="info">
+                    </div>
+                    <div className="info-last">
                         <h3>is VC Funding Drying Up?</h3>
                         <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
-                    </di>
+                    </div>
                     {/* COL-2 END */}
                 </div>
 
