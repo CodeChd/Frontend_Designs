1d126c591d0ce84ab1db31ab1c3e94c174d354a3
CodeChd <Drowzyking1126@gmail.com>
2023-03-14 21:45:23 +0800

    Base

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Section from '../components/Section'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main className='content'>
+        <img src="/images/image-product-mobile.jpg" alt="product-card" />
+        <Section/>
+      </main>
+    </div>
+  )
+}
+
+export default App
