b7c9aadb5bb1b9d87af5d41820a8ae32a57875b7
CodeChd <Drowzyking1126@gmail.com>
2023-03-14 22:18:48 +0800

    Mobile View Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Section from '../components/Section'
+import { ReactComponent as Cart } from '../public/images/iconcart.svg'
 import './App.css'
 
 function App() {
@@ -7,7 +7,17 @@ function App() {
     <div className="App">
       <main className='content'>
         <img src="/images/image-product-mobile.jpg" alt="product-card" />
-        <Section/>
+        <section>
+          <p>PERFUME</p>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>A floral, solar and voluptuous interpretation composed by Olivier Polge,
+            Perfumer-Creator for the House of CHANEL.</p>
+          <h2>
+            $149.99
+            <span>$169.99</span>
+          </h2>
+          <button><Cart /> <span>Add to Cart</span> </button>
+        </section>
       </main>
     </div>
   )
