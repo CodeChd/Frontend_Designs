fbf1de78f179e6cc6a7108304bf6998ed7959129
CodeChd <Drowzyking1126@gmail.com>
2023-03-15 18:11:37 +0800

    Product-Card Design Complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   return (
     <div className="App">
       <main className='content'>
-        <img src="/images/image-product-mobile.jpg" alt="product-card" />
-        <section>
+        <picture>
+          <source srcSet='/images/image-product-desktop.jpg'media="(min-width: 600px  )"/>
+          <img src="/images/image-product-mobile.jpg" alt="" />
+        </picture>
+        <article>
           <p>PERFUME</p>
           <h1>Gabrielle Essence Eau De Parfum</h1>
           <p>A floral, solar and voluptuous interpretation composed by Olivier Polge,
@@ -17,7 +20,7 @@ function App() {
             <span>$169.99</span>
           </h2>
           <button><Cart /> <span>Add to Cart</span> </button>
-        </section>
+        </article>
       </main>
     </div>
   )
