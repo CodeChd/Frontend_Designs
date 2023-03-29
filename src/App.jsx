a3e7eca11e584908c62677c185c6af7ecdcaecbf
CodeChd <Drowzyking1126@gmail.com>
2023-03-30 07:43:02 +0800

    Advice-generator-app finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  useEffect(() => {Advice()},[])
+
+
+  async function Advice () {
+     const data = await axios.get("https://api.adviceslip.com/advice")
+     const res = data.data.slip
+     setData(res)
+
+    //  console.log(data.data.slip)
+
+       }
+
+  const handleClick = () => Advice()
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <main className='content'>
+        <p className='advice-id'>Advice #{data.id} </p>
+        <p className='advice-txt'> "{data.advice}" </p>
+
+        <picture className='divider'>
+        <source srcSet='/images/pattern-divider-desktop.svg' media='(min-width: 400px)'/>
+        <img src="/images/pattern-divider-mobile.svg" alt="" />
+        </picture>
+
+        <button className="dice-btn" onClick={handleClick}>
+        <img className='dice' src="/images/icon-dice.svg" alt="" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
+
     </div>
   )
 }
